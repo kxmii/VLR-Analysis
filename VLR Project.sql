1c21b1a1eb5b9230125b7e5a215b17d4be87adb7
Will <126708296+kxmii@users.noreply.github.com>
2023-03-17 19:38:46 -0700

    Update VLR Project.sql

--- VLR Project.sql
+++ VLR Project.sql
@@ -1,33 +1,33 @@
+--players with the highest avg combat score
 select player, acs
 from [VLR Project]..players
 order by acs desc
---players with the highest avg combat score
 
+--players with at least 100 rounds played
 select player, rnd, acs
 from [VLR Project]..players
 where rnd>100
 order by acs desc
---players with at least 100 rounds played
 
+--players with highest kd
 select player, acs, "K D"
 from [VLR Project]..players
 order by 3 desc
---players with highest kd
 
+--best aimers with at least 100 rounds played
 select player, "hs%"
 from [VLR Project]..players
 where rnd>100
 order by 2 desc
---best aimers with at least 100 rounds played
 
+--teams with best aimers
 select avg(cast(replace("hs%", '%', '') as int)) as HS, team
 from [VLR Project]..players
 group by team
 order by hs desc
---teams with best aimers
 
+--teams with highest avg combat score
 select avg(cast(acs as decimal)) as acs, team
 from [VLR Project]..players
 group by team
 order by acs desc
---teams with highest avg combat score
